c26b8e757bcefa071f65d235e56b33bdcd803b8c
Kauress <kauress8@gmail.com>
2020-09-14 20:14:10 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,13 @@ router.post('/sum', function(req,res){
   const num1 =Number(req.body.number1);
   const num2 =Number(req.body.number2);
   const sum = num1/num2
+  if(num2 = 0){
+    res.render("error", {errormessage: "cannot divide by 0"}
+  } else {
    res.render("success",  {result: sum})
    console.log(req.body);
    //res.end(JSON.stringify(sum));
+  }
 })
 
 router.get("/*", function(req, res) {
